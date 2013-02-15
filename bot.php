cce16d7a6b8a40d7e3294750340c6007e60619de
Jeandre Henderson <theblazehen@gmail.com>
2013-02-15 11:13:44 +0200

    added ability to determine to buy or sell using last 5 measures. UNTESTED!

--- bot.php
+++ bot.php
@@ -6,36 +6,38 @@ $vwap = "100.0";
 $run = "0";
 
 while(true){
-	$run = $run + "1";
+	$run = $run + 1;
 	$info = $gox->getInfo();
 	$ticker = $gox->ticker();
 	$btc_balance = floatval($info['Wallets']['BTC']['Balance']['value']);
 	$usd_balance = floatval($info['Wallets']['USD']['Balance']['value']);
 	/*give balances*/ echo("BTC Balance: " . $btc_balance . "\nUSD Balance: " . $usd_balance . "\n");
-
 	$high = $ticker['ticker']['high'];
 	$low = $ticker['ticker']['low'];
 	$last = $ticker['ticker']['last'];
 
+	$change[1] = $change[2];
+	$change[2] = $change[3];
+	$change[3] = $change[4];
+	$change[4] = $change[5];	
+
 	$oldvwap = $vwap;
 	$vwap = $ticker['ticker']['vwap']; //average price
 	$width = $high - $low;
-	$girth = 1 - ($low / $high);
-	$high_scrape = ( $high - $last ) / $width;
-	$low_scrape  = ( $last - $low ) / $width;
-	$change = $vwap-$oldvwap;
-	passthru(clear);
+	
+	$change[5] = $vwap-$oldvwap;
+	passthru(clear); //clear screen using system command
 	echo("BTC Balance: " . $btc_balance . "\nUSD Balance: $" . $usd_balance . "\n");
 	echo("\nAverage: $". $vwap . "\nHigh: $" . $high . "\nLow: $" . $low . "\nLast: $" . $last);
 	echo "\nRun: " . $run . "\nPrice Change: " . $change ;
 	
 
-	if($vwap > $oldvwap*1.01){
+	if($change[1] > $change[2] && $change[2] > $change[3] && $change[3] > $change[4] && $change[4] > $change[5] ){
 		var_dump($gox->sellBTC($btc_balance,$vwap,"BTC" . $vwap . " per BTC."));
 		echo("\nSold " . $btc_balance . "BTC at $");
 		//echo "$oldvwap*1.05: " . $oldvwap*1.05;
 	}
-	if($vwap < $oldvwap*1.01){
+	if($change[1] < $change[2] && $change[2] < $change[3] && $change[3] < $change[4] && $change[4] < $change[5] ){
 		$gox->buyBTC($usd_balance/$vwap,$vwap,"BTC");
 		echo("\nBought " . $usd_balance/$vwap . "BTC at $" . $vwap . " per BTC.");
 		//echo "\noldvwap*1.05: " . $oldvwap*1.05;
@@ -45,3 +47,4 @@ while(true){
 }
 
 ?>
+
